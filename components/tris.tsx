6561e989f32c45af47ced2a54837b88da7c05d2d
stefanoyoyo <git.stefano@outlook.it>
2022-04-15 17:47:47 +0200

    1 file modified

--- components/tris.tsx
+++ components/tris.tsx
@@ -3,6 +3,10 @@ import React=require("react");
 export class Tris extends React.Component {
   props: unknown;
 
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return <button>ciao</button>
   }
