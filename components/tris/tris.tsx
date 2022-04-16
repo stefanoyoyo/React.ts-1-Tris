a45f1b66c8457f7c6b13c9511739ce706e819342
stefanoyoyo <git.stefano@outlook.it>
2022-04-16 18:30:06 +0200

    Added some style

--- components/tris/tris.tsx
+++ components/tris/tris.tsx
@@ -12,11 +12,11 @@ export class Tris extends React.Component {
     /* Implementing angular *ngFor creating an array 
     having the given number of elements and then using 
     the map method on it. */
-    const els = Array(this.props.trisNumber).fill('empty');
+    const els = Array(this.props.trisNumber).fill('');
     return  (
       <div className="tris-grid">
         {
-          els.map((el, index) => <button>{els[index]}</button> )
+          els.map((el, index) => <button className="square">{els[index]}</button> )
         }
       </div>
     )
